1e7e8da2b4b863a048c9e35a36076572596b525c
Rhian Davies <thetrianglegirl@gmail.com>
2025-08-22 11:49:51 +0100

    🔥 Delete public

--- public/js/accordion.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('.accordion-toggle').forEach(btn => {
-    btn.addEventListener('click', function() {
-      this.classList.toggle('active');
-      let content = this.nextElementSibling;
-      content.style.display = content.style.display === 'block' ? 'none' : 'block';
-    });
-  });
-});
